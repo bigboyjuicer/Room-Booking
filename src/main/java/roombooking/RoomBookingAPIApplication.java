2419077d4b7a554a1b6728902422ee6a7c441808
Maksim Zinin <m.zinin@inbox.ru>
2024-10-30 00:35:11 +0300

    Project creation

---
+++ src/main/java/roombooking/RoomBookingAPIApplication.java
@@ -0,0 +1,13 @@
+package roombooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomBookingAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomBookingAPIApplication.class, args);
+	}
+
+}
